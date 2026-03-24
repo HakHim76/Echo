f239b379979053ba3fb99585c51daa660e1c7908
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-24 23:01:22 +0000

    deploying to sevalla

--- server/src/server.js
+++ server/src/server.js
@@ -4,12 +4,26 @@ dotenv.config();
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", messageRoutes);
 
 const PORT = process.env.PORT;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`listening on port:${PORT}`);
