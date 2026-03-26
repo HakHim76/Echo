60408458d65fa69b0d45c187820e437413bb6a29
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-26 09:30:03 +0000

    signup controller added

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,42 @@
 import dotenv from "dotenv";
-
 dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.json());
+const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/auth", messageRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../client/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
   });
 }
 
-app.listen(PORT, () => {
-  console.log(`listening on port:${PORT}`);
-});
+async function startServer() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("mongo up and running");
+
+    app.listen(PORT, () => {
+      console.log(`listening on port:${PORT}`);
+    });
+  } catch (error) {
+    console.log("mongo connection failed miserably:", error.message);
+    process.exit(1);
+  }
+}
+
+startServer();
