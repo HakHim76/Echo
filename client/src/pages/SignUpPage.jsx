974c4d991f64e85d19bf9f7e9f4ca856da7e9634
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-30 16:26:39 +0000

    dotenv bug fix

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore'
 
 function SignUpPage() {
+const [ formData, setFormData] =useState({
+  fullName:"",email:"",password:""
+})
+
+const {signup, isSigningUp} = useAuthStore()
+
+const handleSubmit = (e)=>{}
+
   return (
-    <div>
-      <h1>Signup</h1>
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+    <div className="relative w-full max-w-6x md:h-[800px] h-[650px] ">
+
+    </div>
     </div>
   )
 }
--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -1,14 +1,40 @@
 import {create} from "zustand"
-
+import { axiosInstance } from "../components/lib/axios"
+import { signup } from "../../../server/src/controllers/auth.controller";
+import toast from "react-hot-toast"
 
 export const useAuthStore = create((set) => ({
-    authUser: {name:"john", _id:123, age:23},
-    isLoggedIn:false,
+   authUser: null,
+   isCheckingAuth:true,
+   isSigningUp : true,
 
-
-
-    login:() =>{
-        console.log("we just logged in")
-        set({ isLoggedIn:true})
-    }
+ checkAuth : async () => {
+      try {
+    const res = await axiosInstance.get("/auth/check");
+    set({ authUser: res.data})
+     } catch (error) {
+    console.log("Error in authCheck:", error)
+    set({authUser: null}) 
+} finally{
+        set({isCheckingAuth: false})
+      }
+    
+ },
 }))
+signup : async  () =>{
+   set({isSigningUp : true})
+try {
+  const res = await axios.post("/auth/signup", data)
+  set({authUser: res.data})
+
+  toast.success("Account created succesfully")
+  
+} catch (error) {
+  toast.error(error.response.data.message)
+  console.log("Error in signup:",error)
+}
+finally{
+  set({isSigningUp:false})
+}
+}
+
