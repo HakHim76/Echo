f514e396f2405e3d3fd9489506ab1d686bab81d7
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-03-30 10:49:11 +0000

    zustand to manage global app data

---
+++ client/src/store/useAuthStore.js
@@ -0,0 +1,14 @@
+import {create} from "zustand"
+
+
+export const useAuthStore = create((set) => ({
+    authUser: {name:"john", _id:123, age:23},
+    isLoggedIn:false,
+
+
+
+    login:() =>{
+        console.log("we just logged in")
+        set({ isLoggedIn:true})
+    }
+}))
