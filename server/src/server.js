90e73ffa87da992753eadb4ab7fb70628624c1a4
Abdul Hakeem Bawah <abdulhakeembawah@gmail.com>
2026-04-02 15:56:53 +0000

    sidebar completed

--- server/src/server.js
+++ server/src/server.js
@@ -12,7 +12,7 @@ app.use(cors({
   origin: ENV.CLIENT_URL,
   credentials: true
 }));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use(cookieParser())
 const __dirname = path.resolve();
